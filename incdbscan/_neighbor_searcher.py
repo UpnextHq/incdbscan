28d3bf204ce837e02cc031e287f416cc8b929dce
Joe Mifsud <joe@mifsud.me>
2024-05-23 12:00:46 -0700

    feat: more efficient ID mapping

--- incdbscan/_neighbor_searcher.py
+++ incdbscan/_neighbor_searcher.py
@@ -1,11 +1,12 @@
+import multiprocessing
+
 import faiss
 import numpy as np
-from faiss import IndexIDMap
 from opentelemetry import trace
-from sortedcontainers import SortedList
 
 BRUTE_FORCE_CUTOFF = 1000
 
+TOMBSTONE_ID = -1
 
 class NeighborSearcher:
     def __init__(self, radius, num_dims):
@@ -13,48 +14,43 @@ class NeighborSearcher:
 
         self.num_dims = num_dims
 
-        self.values = np.array([])
-        self.ids = SortedList()
+        self.values = np.empty((0, num_dims), dtype=np.float32)
+        self.ids = []
 
         self.neighbor_searcher = None
         self.remake_index()
 
     def insert(self, new_value, new_id):
-        self.ids.add(new_id)
-        position = self.ids.index(new_id)
-
-        self._insert_into_array(new_value, position)
-
         if len(self.values) % BRUTE_FORCE_CUTOFF == 0:
             self.remake_index()
 
+        self.ids.append(new_id)
+
+        new_value = np.array(new_value, dtype=np.float32).reshape(1, -1)
+        faiss.normalize_L2(new_value)
+        self._insert_into_array(new_value)
+
         tracer = trace.get_tracer(__name__)
         with tracer.start_as_current_span('incdbscan_insert_neighborhood_searcher_fit'):
-            new_value = np.array([new_value], dtype=np.float32)
-            faiss.normalize_L2(new_value)
+            self.neighbor_searcher.add(new_value)
 
-            self.neighbor_searcher.add_with_ids(new_value, np.array([new_id], dtype='int64'))
-
-    def _insert_into_array(self, new_value, position):
-        extended = np.insert(self.values, position, new_value, axis=0)
-        if not self.values.size:
-            extended = extended.reshape(1, -1)
-        self.values = extended
+    def _insert_into_array(self, new_value):
+        self.values = np.vstack((self.values, new_value))
 
     def query_neighbors(self, query_value):
         query_value = np.array([query_value], dtype=np.float32)
         faiss.normalize_L2(query_value)
 
-        _, _, neighbors = self.neighbor_searcher.range_search(query_value, self.radius)
-        for n_id in neighbors:
-            yield self.ids[self.ids.index(n_id)]
+        _, _, neighbors = self.neighbor_searcher.range_search(query_value, 1 - self.radius)
+        for n_idx in neighbors:
+            if self.ids[n_idx] == TOMBSTONE_ID:
+                continue
+
+            yield self.ids[n_idx]
 
     def delete(self, id_):
-        self.neighbor_searcher.remove_ids(np.array([id_], dtype=np.int64))
-
-        position = self.ids.index(id_)
-        del self.ids[position]
-        self.values = np.delete(self.values, position, axis=0)
+        pos = self.ids.index(id_)
+        self.ids[pos] = TOMBSTONE_ID
 
     def remake_index(self):
         if len(self.ids) < BRUTE_FORCE_CUTOFF:
@@ -67,4 +63,20 @@ class NeighborSearcher:
 
             new_index.train(self.values)
 
-        self.neighbor_searcher = IndexIDMap(new_index)
+        self.neighbor_searcher = new_index
+
+        orig_ids = self.ids
+        orig_values = self.values
+
+        self.values = np.empty((0, self.num_dims), dtype=np.float32)  # Ensure values is reset with the correct shape
+        self.ids = []
+
+        for orig_id, orig_val in zip(orig_ids, orig_values):
+            if orig_id == TOMBSTONE_ID:
+                continue
+
+            self._insert_into_array(orig_val)
+            self.ids.append(orig_id)
+
+        if self.values.size > 0:
+            self.neighbor_searcher.add(self.values)
